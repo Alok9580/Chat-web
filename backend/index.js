03531d9726b1e25b36feb4ddcef091ee1be186b0
Alok9580 <alok1singh5660@gmail.com>
2024-10-10 16:12:14 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,12 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+
 mongoose.connect("mongodb://localhost:27017/company", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
+
+
 const port = 3000;
 const bcryptSalt = bcrypt.genSaltSync(10);
 app.get('/test', (req, res) => {
